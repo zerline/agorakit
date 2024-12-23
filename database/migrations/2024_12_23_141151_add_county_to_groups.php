f6cd8735fd2e96ad68e8256692207edc36a5ddcc
Odile Bénassy <zerline@fluidinfo.fr>
2024-12-23 17:45:30 +0100

    Add county information.

---
+++ database/migrations/2024_12_23_141151_add_county_to_groups.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddCountyToGroups extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('groups', function ($table) {
+            $table->string('county'); // Keep it flexible
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            $table->dropColumn('county');
+        });
+    }
+}
